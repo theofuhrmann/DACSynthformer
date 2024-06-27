41f312ed71b67ea45821f468ac47506e7e40ed00
lonce <lonce.acad@zwhome.org>
2024-06-27 23:35:38 +0800

    just a bit of cleanup

--- DACTransformer/DACTransformer.py
+++ DACTransformer/DACTransformer.py
@@ -84,26 +84,6 @@ class TransformerBlock(nn.Module):
         return src
     
     
-# class TransformerBlock(nn.Module):
-#     def __init__(self, embed_size, forward_expansion, heads, dropout):
-#         super().__init__()
-#         self.attention = nn.MultiheadAttention(embed_dim=embed_size, num_heads=heads, dropout=dropout, batch_first=True)
-#         self.norm1 = nn.LayerNorm(embed_size)
-#         self.feed_forward = nn.Sequential(
-#             nn.Linear(embed_size, forward_expansion * embed_size),
-#             nn.ReLU(),
-#             nn.Linear(forward_expansion * embed_size, embed_size)
-#         )
-#         self.dropout = nn.Dropout(dropout)
-#         self.norm2 = nn.LayerNorm(embed_size)
-
-#     def forward(self, src, mask):
-#         attn_output, _ = self.attention(src, src, src, attn_mask=mask)
-#         src = self.norm1(src + self.dropout(attn_output))
-#         ff_output = self.feed_forward(src)
-#         src = self.norm2(src + self.dropout(ff_output))
-#         return src
-    
 #-------------------------------------------------------------------
 
 class TransformerDecoder(nn.Module):
@@ -135,9 +115,6 @@ class TransformerDecoder(nn.Module):
         
         if (self.verbose > 5) :
             print(f'In TransformerDecoder, before concating cond, source.shape is {src.shape}')
-        
-        # Expand and concatenate conditioning vector for the initial input
-        #----- cond_expanded = cond.unsqueeze(1).expand(-1, src.size(1), -1)
         
         if cond_expanded != None : 
             if (self.verbose > 5) :
@@ -174,30 +151,3 @@ class TransformerDecoder(nn.Module):
         return logits.view(src.shape[0], src.shape[1], -1, 1024)  # Adjust reshape as needed
     
     
-# class TransformerDecoder(nn.Module):
-#     def __init__(self, embed_size, num_layers, heads, forward_expansion, dropout, max_len, num_codebooks, vocab_size):
-#         super().__init__()
-        
-#         self.embed_size=embed_size
-#         self.num_layers=num_layers
-#         self.heads=heads
-#         self.forward_expansion=forward_expansion
-#         self.dropout=dropout
-#         self.max_len=max_len  # used for rotary encoder
-#         self.num_codebooks=num_codebooks
-#         self.vocab_size=vocab_size
-        
-#         self.embed = MultiEmbedding(vocab_size, embed_size//num_codebooks, num_codebooks)  #2nd arg is embedding size per token
-#         self.pos_encoder = RotaryPositionalEmbedding(embed_size)
-#         self.layers = nn.ModuleList([
-#             TransformerBlock(embed_size, forward_expansion, heads, dropout) for _ in range(num_layers)
-#         ])
-#         self.output_layer = nn.Linear(embed_size, num_codebooks * vocab_size)
-
-#     def forward(self, src, src_mask):
-#         src = self.embed(src)
-#         src = self.pos_encoder(src)
-#         for layer in self.layers:
-#             src = layer(src, src_mask)
-#         logits = self.output_layer(src)
-#         return logits.view(src.shape[0], src.shape[1], -1, 1024)  # Reshape for individual token vocab
